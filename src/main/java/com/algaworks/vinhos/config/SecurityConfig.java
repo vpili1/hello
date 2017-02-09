a4d80ab5a12102f44f9ac46e5cd3c358d1a0254d
Kimberlly Moreles <Kimberlly Moreles>
2017-02-09 21:28:33 -0200

    Segurança

--- src/main/java/com/algaworks/vinhos/config/SecurityConfig.java
+++ src/main/java/com/algaworks/vinhos/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.algaworks.vinhos.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,5 +22,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring()
 			.antMatchers("/layout/**");
 	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/vinhos").hasRole("PESQUISAR_VINHO")
+				.antMatchers("/vinhos/**").hasRole("CADASTRAR_VINHO")
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}	
 	
 }
